b6eca040277e3f72fde271911efcd6b0e08bb218
Amit8185 <149670584+Amit8185@users.noreply.github.com>
2025-08-12 16:13:58 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -1,9 +1,5 @@
-# vulnerable.py
+import subprocess
 
-# Example of a hardcoded password (Semgrep will catch this)
-
-
-def login(user, pwd):
-    if user == "admin" and pwd == "1234":
-        return "Access granted"
-    return "Access denied"
+def run_command(user_input):
+    # Dangerous use of shell=True leading to command injection vulnerability
+    subprocess.call(user_input, shell=True)
